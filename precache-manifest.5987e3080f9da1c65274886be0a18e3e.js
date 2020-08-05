c5ee5a28bf96565cd1e2998dfe79849e684a39c1
lokeshn011101 <lokesh19055@cse.ssn.edu.in>
2020-08-05 14:40:58 +0530

    Updates

---
+++ precache-manifest.5987e3080f9da1c65274886be0a18e3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "857af3890257d2742d60c934e2dda9b8",
+    "url": "/Dictionary/index.html"
+  },
+  {
+    "revision": "f7e2ca05ff953a361456",
+    "url": "/Dictionary/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "43f4c7a50d3b3d441128",
+    "url": "/Dictionary/static/css/main.b8e6a094.chunk.css"
+  },
+  {
+    "revision": "f7e2ca05ff953a361456",
+    "url": "/Dictionary/static/js/2.b7327fb0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Dictionary/static/js/2.b7327fb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43f4c7a50d3b3d441128",
+    "url": "/Dictionary/static/js/main.d04657d1.chunk.js"
+  },
+  {
+    "revision": "aaae350361e8996d7a9c",
+    "url": "/Dictionary/static/js/runtime-main.b151d5db.js"
+  }
+]);
