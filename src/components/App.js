c2485eb00314f86fc60e4d2e5de63f41ee7be255
lokeshn011101 <lokesh19055@cse.ssn.edu.in>
2020-08-06 17:27:34 +0530

    particlejs animation added

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ import PronunData from "../components/Data/mainContainer/Pronunciations/pronunDa
 import history from "../history";
 import Error from "./Error";
 import "./App.css";
+import Particles from "react-particles-js";
 
 const LineLoader = () => {
   return <div className="lineloader"></div>;
@@ -21,6 +22,59 @@ class App extends Component {
     }
     return (
       <div className="container">
+        <Particles
+          params={{
+            particles: {
+              number: {
+                value: 160,
+                density: {
+                  enable: false,
+                },
+              },
+              size: {
+                value: 3,
+                random: true,
+                anim: {
+                  speed: 4,
+                  size_min: 0.3,
+                },
+              },
+              line_linked: {
+                enable: false,
+              },
+              move: {
+                random: true,
+                speed: 1,
+                direction: "top",
+                out_mode: "out",
+              },
+            },
+            interactivity: {
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: "bubble",
+                },
+                onclick: {
+                  enable: true,
+                  mode: "repulse",
+                },
+              },
+              modes: {
+                bubble: {
+                  distance: 250,
+                  duration: 2,
+                  size: 0,
+                  opacity: 0,
+                },
+                repulse: {
+                  distance: 400,
+                  duration: 4,
+                },
+              },
+            },
+          }}
+        />
         <Router history={history}>
           <div className="app-container">
             <Header />
